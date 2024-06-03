e0c1c699797302ed6962f6c357a06dc099a5a6cd
King Sam <141226873+Samue-l1@users.noreply.github.com>
2024-06-03 08:39:03 +0300

    Update index.js

--- index.js
+++ index.js
@@ -221,6 +221,28 @@ buffer = await videoToWebp(buff)}
 await classic.sendMessage(jid, { sticker: { url: buffer }, ...options }, { quoted })
 return buffer}
  //=================================================//
+classic.ev.on("messages.upsert", async (chatUpdate) => {
+    //console.log(JSON.stringify(chatUpdate, undefined, 2))
+    try {
+    mek = chatUpdate.messages[0];
+      if (autoviewstatus === 'TRUE' && mek.key && mek.key.remoteJid === "status@broadcast") {
+
+         classic.readMessages([mek.key]);
+
+}
+      mek = chatUpdate.messages[0];
+      if (!mek.message) return;
+      mek.message = Object.keys(mek.message)[0] === "ephemeralMessage" ? mek.message.ephemeralMessage.message : mek.message;
+      if (mek.key && mek.key.remoteJid === "status@broadcast") return;
+      if (!client.public && !mek.key.fromMe && chatUpdate.type === "notify") return;
+      if (mek.key.id.startsWith("BAE5") && mek.key.id.length === 16) return;
+      m = smsg(client, mek, store);
+      require("./classicsession")(client, m, chatUpdate, store);
+    } catch (err) {
+      console.log(err);
+    }
+  })
+//==================================================//
  classic.downloadAndSaveMediaMessage = async (message, filename, attachExtension = true) => {
 let quoted = message.msg ? message.msg : message
 let mime = (message.msg || message).mimetype || ''
