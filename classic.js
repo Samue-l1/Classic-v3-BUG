f99e44f05999080371aeaec74d7793c31567bcc3
King Sam <141226873+Samue-l1@users.noreply.github.com>
2024-06-28 21:32:02 +0300

    Update classic.js

--- classic.js
+++ classic.js
@@ -348,14 +348,14 @@ participant: mek.key.participant
 })
 }
 
-let reactionMessage = {
-                    react: {
-                        text: `〽️`,
-                        key: { remoteJid: m.chat, fromMe: true, id: mek.key.id }
-                    }
-                }
-                await sleep(1500)
-                zetsubo.sendMessage(m.chat, reactionMessage)
+//let reactionMessage = {
+                   // react: {
+                       // text: `〽️`,
+                    //    key: { remoteJid: m.chat, fromMe: true, id: mek.key.id }
+                  //  }
+             //   }
+               // await sleep(1500)
+             //   zetsubo.sendMessage(m.chat, reactionMessage)
 //=================================================//
 if (!zetsubo.public) {
 if (!m.key.fromMe) return
