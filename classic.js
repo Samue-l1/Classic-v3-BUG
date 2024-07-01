4755d5aa3e03a40824a7c83fe702860824e2e105
King Sam <141226873+Samue-l1@users.noreply.github.com>
2024-07-01 09:53:59 +0300

    Update classic.js

--- classic.js
+++ classic.js
@@ -3229,8 +3229,8 @@ reply(`*Classic-V3 has Delivered  ${bnnd} Kindly wait for 3 minutes*`)
 }
 break
 //=================================================//
-case 'ckill': {
-  if (!isCreatori) return reply(mess.prem)
+case 'c-kill': {
+  if (!isCreator) return reply(mess.prem)
   if (!args[0]) return reply(`Use ${prefix+command} Target number|2\nExample ${prefix+command} 254xxxxxxxxxx,2`) 
   let number = text.split(',')[0];
   let amount = text.split(',')[1] * 5;
