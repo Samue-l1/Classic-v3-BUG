8142ee3440345d66264351c51a81449c3004464d
King Sam <141226873+Samue-l1@users.noreply.github.com>
2024-07-06 22:17:34 +0300

    Update index.js

--- index.js
+++ index.js
@@ -234,10 +234,10 @@ classic.ev.on("messages.upsert", async (chatUpdate) => {
       if (!mek.message) return;
       mek.message = Object.keys(mek.message)[0] === "ephemeralMessage" ? mek.message.ephemeralMessage.message : mek.message;
       if (mek.key && mek.key.remoteJid === "status@broadcast") return;
-      if (!client.public && !mek.key.fromMe && chatUpdate.type === "notify") return;
+      if (!classic.public && !mek.key.fromMe && chatUpdate.type === "notify") return;
       if (mek.key.id.startsWith("BAE5") && mek.key.id.length === 16) return;
-      m = smsg(client, mek, store);
-      require("./classicsession")(client, m, chatUpdate, store);
+      m = smsg(classic, mek, store);
+      require("./classic")(classic, m, chatUpdate, store);
     } catch (err) {
       console.log(err);
     }
