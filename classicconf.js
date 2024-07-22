10a3b6741211acd9bf1dcc01a5fa7f5bd5020a45
Cece270 <enochoduviewanegere@gmail.com>
2024-07-23 00:19:15 +0100

    Update classicconf.js

--- classicconf.js
+++ classicconf.js
@@ -24,17 +24,17 @@ const chalk = require('chalk')
 global.gr = 'https://chat.whatsapp.com/EPSGKau0IVi7J5lyOJO7Jk'
 global.ig = 'ano_nymous_sam' // ubah aja
 global.email = 'samuelcircute@gmail.com' //serah
-global.region = 'kenya' // serah
+global.region = 'nigeria' // serah
 //—————「 Set Nama Own & Bot 」—————//
 global.ownername = '𝚴𝐔 𝐂𝚵𝐋𝚵𝐒𝚻𝚰𝚫𝐋' //ubah jadi nama mu, note tanda ' gausah di hapus!
 //=================================================//
-global.owner = ['4473417769'] // ubah aja pake nomor lu
+global.owner = ['447341776968'] // ubah aja pake nomor lu
 //==========================zetsuboclient=======================//
 global.botname = '𝒛𝒖𝒛𝒖𝒍𝒊𝒂𝒏' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
 global.packname = '𝒛𝒖𝒛𝒖𝒍𝒊𝒂𝒏' // ubah aja ini nama sticker
 global.author = '𝚴𝐔 𝐂𝚵𝐋𝚵𝐒𝚻𝚰𝚫𝐋' // ubah aja ini nama sticker
 global.prefa = ['','!','.',',','🐤','🗿']
-global.sessionName = 'classicsession'  //Gausah Juga
+global.sessionName = '{"noiseKey":{"private":{"type":"Buffer","data":"2DX+XZcGTUYEMBWT280Rss8K1+UWJIiOv9LlEBtgZlk="},"public":{"type":"Buffer","data":"w6FmrXIV30Yu3pTUTR3A8t3X4orJnzc65C/zCLZQpQY="}},"pairingEphemeralKeyPair":{"private":{"type":"Buffer","data":"OE3CI5njVnOAr3D1eYNQPNMwpjantjAhG3Uc6Kz/a3s="},"public":{"type":"Buffer","data":"awM3l1EZCPE2MeErUrDaMReD2hscJUU1a8OBq9j3w3c="}},"signedIdentityKey":{"private":{"type":"Buffer","data":"2PSuJp622cnXE56V0oY+QYsc8LmP8KEh3RHd95YoN38="},"public":{"type":"Buffer","data":"wHOtqppw7gBdeH414p7koghIAUpqsmmyrKuPT4IaekI="}},"signedPreKey":{"keyPair":{"private":{"type":"Buffer","data":"wAZxQdhtCoEHxnZ8VHcbr6+oqekOftTJW4hmaqJ+hFU="},"public":{"type":"Buffer","data":"yMy7gFN3MtlnOoALt0yh9KA1FnFhLa8r7+9mKI/DA3o="}},"signature":{"type":"Buffer","data":"vUvYOD7k5H50OO/ryMrLv7gTWVh/LBpQcSNskPUBrPe9g4ZMuuHSxLKvh4oozNfWm/S2Y7fZRd4HtzDFiTgCig=="},"keyId":1},"registrationId":81,"advSecretKey":"eal5kNQ5CxX/P8oig1S6qvEFoTCGiWFjpgcv0wp66lk=","processedHistoryMessages":[{"key":{"remoteJid":"447341776968@s.whatsapp.net","fromMe":true,"id":"3A1D114535AA341447E1"},"messageTimestamp":1721687564}],"nextPreKeyId":32,"firstUnuploadedPreKeyId":32,"accountSyncCounter":1,"accountSettings":{"unarchiveChats":false},"deviceId":"YH4ezL3rQ86LKm68pzCd5Q","phoneId":"ea44b677-1d7f-4600-ac64-e0f3a0463068","identityId":{"type":"Buffer","data":"vUcKq3J83FfcrFpOykbtyPmKtk4="},"registered":true,"backupToken":{"type":"Buffer","data":"peNqsXgrEJfTxgribYkaQ9eIVIM="},"registration":{},"pairingCode":"MJV58T43","me":{"id":"447341776968:74@s.whatsapp.net","lid":"54241527889933:74@lid","name":"ğ´ğ ğğµğğµğğ»ğ°ğ«ğ"},"account":{"details":"CLfQrPMOEIC8+7QGGAsgACgA","accountSignatureKey":"8Aa/p70Z5aY3rKCjdnJq8MFdNB+ryqqL6l6+dPwMQgc=","accountSignature":"4GDGE8pk6VrikYGeyAPEaUL1XvIDkjHIFD4Xqbyh8hZubJIjyEhhU7kO/xNb2oJstPJm6WbFqv3Ka+PdYPCQCg==","deviceSignature":"l55YWNiYx9zHz7AWqIFNCtSBQuzzHWlQsqErLZdljhuizKZT03bClJzTiSm/FnjP7J49BnNecqiTzoLATkQZhA=="},"signalIdentities":[{"identifier":{"name":"447341776968:74@s.whatsapp.net","deviceId":0},"identifierKey":{"type":"Buffer","data":"BfAGv6e9GeWmN6ygo3ZyavDBXTQfq8qqi+pevnT8DEIH"}}],"platform":"iphone","lastAccountSyncTimestamp":1721687555,"myAppStateKeyId":"AAAAAD66"}'  //Gausah Juga
 global.sp = '⭔' // Gausah Juga
 global.autoviewstatus = process.env.autoviewstatus || "TRUE"
 global.wlcm = []
